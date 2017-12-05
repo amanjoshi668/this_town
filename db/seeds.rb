184251acaa8b565acabd1df26035a8434ef28bff
Aman Joshi <amanjoshi668@gmail.com>
2017-12-05 16:53:04 +0530

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,13 @@ User.create!(name:  "Example User",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+User.create!(name: "Aman Joshi",
+						 email: "amanjoshi668@gmail.com",
+						 password: 							"Amanricha668@",
+						 password_confirmation: "Amanricha668@",
+						 admin: true,
+						 activated: true,
+						 activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -29,3 +36,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
